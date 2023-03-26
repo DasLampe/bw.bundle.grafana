dc93bb147f48a3c75656852a9fa637d3573407b0
DasLampe <daslampe@lano-crew.org>
2023-03-26 16:53:28 +0200

    Add: Auth via GitHub

--- metadata.py
+++ metadata.py
@@ -6,6 +6,7 @@ defaults = {
         'admin_username': 'admin',
         'admin_password': repo.vault.password_for("grafana_admin_{}".format(node.name)),
         'admin_email': 'admin@localhost',
+        'secret_key': repo.vault.password_for(f'grafana_secret_key_{node.name}'),
         'create_database': True,
         'database':  {
             'host': '127.0.0.1',
@@ -15,8 +16,29 @@ defaults = {
             'password': repo.vault.password_for("postgres_user_{}_{}".format('grafana', node.name)),
         },
         'http': {
+            'domain': 'localhost',
+            'root_url': '%(protocol)s://%(domain)s:%(http_port)s/',
             'addr': '127.0.0.1',
             'port': '3000',
+        },
+        'auth': {
+            'github': {
+                'enabled': False,
+                'allow_sign_up': True,
+                'auto_login': False,
+                'client_id': '',
+                'client_secret': '',
+                'scopes': ['user:email', 'read:org'],
+                'auth_url': 'https://github.com/login/oauth/authorize',
+                'token_url': 'https://github.com/login/oauth/access_token',
+                'api_url': 'https://api.github.com/user',
+                'allowed_domains': [],
+                'team_ids': [],
+                'allowed_organizations': [],
+                'role_attribute_path': '',
+                'role_attribute_strict': False,
+                'allow_assign_grafana_admin': False,
+            }
         },
     },
     'apt': {
